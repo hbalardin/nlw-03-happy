df953ed115fabe7a4a79f7bde4d7cc0d9f7c8da3
Henrique Balardin <balardinh@gmail.com>
2020-10-17 16:41:57 -0300

    chore: implemented new routes and createOrphanage pages

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -6,14 +6,24 @@ import { createStackNavigator } from '@react-navigation/stack';
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
 
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageDetails';
+
 const { Navigator, Screen } = createStackNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: '#f2f3f5' },
+        }}
+      >
         <Screen name="OrphanagesMap" component={OrphanagesMap} />
         <Screen name="OrphanageDetails" component={OrphanageDetails} />
+        <Screen name="SelectMapPosition" component={SelectMapPosition} />
+        <Screen name="OrphanageData" component={OrphanageData} />
       </Navigator>
     </NavigationContainer>
   );
